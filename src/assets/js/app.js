c92db77e8128eb18589ab505a3d5780297c022ab
vikok <vikskopkop@gmail.com>
2017-03-01 10:04:53 +0200

    add href

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,2 +1,12 @@
 $(document).foundation();
 
+	$(".toggle_menu").click(function(){
+	var menu = $(".top-menu");
+	if (menu.hasClass('open')){
+	menu.css('display', "none");
+	menu.removeClass('open');
+	} else {
+	menu.css('display', "block");
+	menu.addClass('open');
+	}
+	});
